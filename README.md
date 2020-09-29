1c2856fcaed1fa84e901ae01a3fb89cf5332809e
RafaLinaresMolina <rafa.linares.molina@gmail.com>
2020-09-29 19:28:42 +0200

    added feature to paint the logger in both, browser or terminal (nodejs)

--- README.md
+++ README.md
@@ -3,45 +3,47 @@
 Just a simple js file for make the console.log a bit more colorfull and readable on the browser.
 
 ## Config
-For the moment the config is in the same js file.
+```diff
++ Now you have a config.json file!
+```
+The config file has:
 
 - minimumLevelLog: is used to specify how much level of console log you want to show, being right now:
   - [0] Error: Pretty staightforward, something blowed up.
   - [1] Warning: Just tell you that might be something problematic.
   - [2] Data: For visualize objects and stuff.
   - [3] Info: Inform you were is going the flow of the execution.
+- dateIso: Indicates if you want the date to be display in ISO format or default format, being default like this: [ 29-9-2020 16:40:23 ]
 
-And the config object is something like that:
-```
+```json
 {
-  0: {
-    style: "color: red; font-weight: bold;",
-    header: "[ " + date.toISOString() + " ] † ERROR: ",
-  },
-  1: {
-    style: "color: orange; font-weight: bold;",
-    header: "[ " + date.toISOString() + " ] ▲ WARNING: ",
-  },
-  2: {
-    style: "color: lightgreen; font-weight: bold;",
-    header: "[ " + date.toISOString() + " ] ► DATA: ",
-  },
-  3: {
-    style: "color: lightblue; font-weight: bold;",
-    header: "[ " + date.toISOString() + " ] • INFO: ",
-  },
+	"minimumLevelLog": 3,
+	"dateIso": true,
+	"0": {
+		"frontStyle": "color: red; font-weight: bold;",
+		"header": "[ ##DATE## ] † ERROR: ",
+		"nodeFontColor": "[31m"
+	},
+	"1": {
+		"frontStyle": "color: orange; font-weight: bold;",
+		"header": "[ ##DATE## ] ▲ WARNING: ",
+		"nodeFontColor": "[33m"
+	},
+	"2": {
+		"frontStyle": "color: lightgreen; font-weight: bold;",
+		"header": "[ ##DATE## ] ► DATA: ",
+		"nodeFontColor": "[32m"
+	},
+	"3": {
+		"frontStyle": "color: lightblue; font-weight: bold;",
+		"header": "[ ##DATE## ] • INFo: ",
+		"nodeFontColor": "[34m"
+	}
 }
 ```
 
 ## Usage
-
-### IMPORTANT 
-```diff
-# The new version changed the way manages the call to logger.
-- logger({ type: 0, msg: "Happy litle error!" });
-# Changed to 
-+ logger(0, "Happy litle error!");
-```
+Import the module and just type ``` logger(0, "Happy litle error!"); ```
 
 Just download the js file, place it on your project and import the file in your js files like this:
 ```
@@ -54,8 +56,12 @@ logger(1, "Contraseña no segura!");
 logger(2, jsonObj);
 logger(3, "Entrada en el metodo de loggin");
 ```
-And this will be the output:
+This will be the output on the browser:
 
 ![Final result](final-result.png)
+
+And this will be the output on the terminal:
+
+![Final result](final-result-node.png)
 
 hope this is useful for someone.
--- logger.js
+++ logger.js
@@ -1,39 +1,36 @@
-// constants for default values
-const date = new Date();
-// TODO Export it to a config file
-let minimumLevelLog = 3;
-const stylePrefix = "%c";
-
-/*
-  JSON object with the basic config for the logs
-  style: css code for the message to use
-  header: first part of the message, it contains:
-    -> [ DATE (ISO format) ] TYPE: 
-*/
-// TODO Export it to a config file
-const errorTypes = {
-  0: {
-    style: "color: red; font-weight: bold;",
-    header: "[ " + date.toISOString() + " ] † ERROR: ",
-  },
-  1: {
-    style: "color: orange; font-weight: bold;",
-    header: "[ " + date.toISOString() + " ] ▲ WARNING: ",
-  },
-  2: {
-    style: "color: lightgreen; font-weight: bold;",
-    header: "[ " + date.toISOString() + " ] ► DATA: ",
-  },
-  3: {
-    style: "color: lightblue; font-weight: bold;",
-    header: "[ " + date.toISOString() + " ] • INFO: ",
-  },
-};
+let minimumLevelLog;
+const frontPrefix = "%c";
+const backPrefix = "\x1b";
+const resetColorOnNode = "\x1b[0m";
+const isNode = typeof process === "object";
+let configValues;
 
 /**
- * Function that return a true if is a string and false if 
+ * Function to retrive the data from the config file
+ * @returns json
+ */
+const readConfig = async () => {
+  let data;
+  if (!isNode) {
+    data = await fetch("./config.json");
+    data = await data.json();
+  } else {
+    const fs = require("fs").promises;
+    data = await fs.readFile("./config.json", "utf-8");
+    data = JSON.parse(data);
+  }
+  minimumLevelLog = data["minimumLevelLog"];
+  return data;
+};
+
+readConfig()
+  .then((data) => (configValues = JSON.parse(data)))
+  .catch((err) => console.log);
+
+/**
+ * Function that return a true if is a string and false if
  * is a well formated JSON object
- * @param {json | string} str 
+ * @param {json | string} str
  * @returns boolean
  */
 const isString = (str) => {
@@ -43,12 +40,12 @@ const isString = (str) => {
     return false;
   }
   return true;
-}
+};
 
 /**
  * Function that will return a formated json or a string.
  * used for pretify JSON objects on the terminal.
- * @param {json | string} json 
+ * @param {json | string} json
  * @returns string
  */
 const syntaxHighlight = (json) => {
@@ -57,23 +54,54 @@ const syntaxHighlight = (json) => {
   } else {
     return json;
   }
-}
+};
 
 /**
  * Method for print with style the message to the terminal.
- * @param {Number} type 
- * @param {String} msg 
+ * @param {Number} type
+ * @param {String} msg
  */
 const logger = (type, msg) => {
-
   // We will ignore the logs if they do not exist and if they are outside the level of interest
-  if (errorTypes[type] && type <= minimumLevelLog) {
-    const formatedMessage =
-      stylePrefix +
-      errorTypes[type].header +
-      syntaxHighlight(msg);
-    console.log(formatedMessage, errorTypes[type].style);
+  if (configValues === undefined) {
+    readConfig().then((data) => {
+      configValues = data;
+      typeMessage(type, msg);
+    });
+  } else {
+    typeMessage(type, msg);
   }
-}
+};
+
+const setHeaderDate = (header, toIso) => {
+  if (configValues["dateIso"])
+    return header.replace("##DATE##", new Date().toISOString());
+  else return header.replace("##DATE##", dateToDefaultFormat(new Date()));
+};
+
+const dateToDefaultFormat = (date) => {
+  return `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
+
+const typeMessage = (type, msg) => {
+  if (configValues[type] && type <= minimumLevelLog) {
+    let formatedMessage = "";
+    if (!isNode) {
+      formatedMessage = `${frontPrefix}${setHeaderDate(
+        configValues[type].header
+      )} ${syntaxHighlight(msg)}`;
+      console.log(formatedMessage, configValues[type].frontStyle);
+    } else {
+      formatedMessage = `${backPrefix}${
+        configValues[type].nodeFontColor
+      } ${setHeaderDate(configValues[type].header)} ${syntaxHighlight(
+        msg
+      )} ${resetColorOnNode}`;
+      console.log(formatedMessage);
+    }
+  }
+};
 
 export default logger;
